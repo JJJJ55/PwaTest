49a80774acc42ae036b372e40628662cec4a4b22
JinY <wlsdud6221@naver.com>
2024-07-16 21:53:44 +0900

    pwa test 240716

--- src/components/CarDamage/RepairShopImg.js
+++
@@ -1,3 +0,0 @@
-export const maker = require('../../assets/img/Maker.png');
-export const Hmaker = require('../../assets/img/Hmaker.png');
-export const Kmaker = require('../../assets/img/Kmaker.png');
--- src/lib/context/LoginContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-export const LoginDispatchContext = createContext();
-
-export const LoginStateContext = createContext();
