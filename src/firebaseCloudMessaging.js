3fc6e01676f0ebf34d646cc977f6bd490cbecf9e
JinY <wlsdud6221@naver.com>
2024-08-13 22:33:33 +0900

    messaging Id 수정

--- src/firebaseCloudMessaging.js
+++ src/firebaseCloudMessaging.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FCM_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FCM_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FCM_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+  messagingSenderId: process.env.REACT_APP_FCM_MESSAGING_SENDERID,
   appId: process.env.REACT_APP_FCM_APPID,
   measurementId: process.env.REACT_APP_FCM_MEASUREMENT_ID,
 };
