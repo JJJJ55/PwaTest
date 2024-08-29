f38557687cfcf2b3fb19b70ee5659206d8620e5f
JinY <wlsdud6221@naver.com>
2024-08-29 17:47:07 +0900

    이미지 메타데이터 확인 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import CameraComponent from "./CameraComponent";
-import CameraApp from "./CameraApp";
-import OcrTest from "./OcrTest";
-import TeachableMachinePose from "./TeachableMachinePose";
-import Model from "./Model";
+import CameraComponent from "./TestComponents/CameraComponent";
+import CameraApp from "./TestComponents/CameraApp";
 import { requestPermission } from "./firebaseCloudMessaging";
+import ImageMeta from "./TestComponents/ImageMeta";
 
 function App() {
   requestPermission();
   return (
     <div className="App">
-      pwa 푸쉬알림 테스트
+      카메라, 푸쉬알림, 이미지 메타정보 테스트
       {/* <CameraComponent /> */}
-      <CameraApp />
-      {/* <OcrTest /> */}
-      {/* <TeachableMachinePose /> */}
-      {/* <Model /> */}
+      {/* <CameraApp /> */}
+      <ImageMeta />
     </div>
   );
 }
