e2e274a26b08008c7a11698f27378029d8b416e1
JinY <wlsdud6221@naver.com>
2024-08-30 12:59:28 +0900

    터치이벤트 제어

--- src/TestComponents/VideoTag.jsx
+++ src/TestComponents/VideoTag.jsx
@@ -3,22 +3,6 @@ import videData from "../asset/메인영상.mp4";
 import "./VideoCss.css";
 
 const VideoTag = () => {
-  // const handleKeyDown = (event) => {
-  //   if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-  //     console.log("멈춰");
-  //     event.preventDefault();
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   // 문서에 키보드 이벤트 리스너 추가
-  //   document.addEventListener("keydown", handleKeyDown);
-
-  //   // 컴포넌트 언마운트 시 이벤트 리스너 제거
-  //   return () => {
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
   const videoRef = useRef(null);
 
   // 포커스를 제거하는 함수
@@ -28,16 +12,25 @@ const VideoTag = () => {
     }
   };
 
+  // 터치 이벤트 처리 함수
+  const handleTouchStart = (event) => {
+    if (videoRef.current) {
+      event.preventDefault(); // 터치 이벤트 방지
+    }
+  };
+
   useEffect(() => {
     // 비디오 요소에 포커스 이벤트 리스너 추가
     const videoElement = videoRef.current;
 
     if (videoElement) {
       videoElement.addEventListener("focus", removeFocus);
+      videoElement.addEventListener("touchstart", handleTouchStart);
 
       // 컴포넌트 언마운트 시 이벤트 리스너 제거
       return () => {
         videoElement.removeEventListener("focus", removeFocus);
+        videoElement.removeEventListener("touchstart", handleTouchStart);
       };
     }
   }, []);
