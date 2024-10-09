e96c1cbac76b89a62d725b68a77755b093c400d7
JinY <wlsdud6221@naver.com>
2024-10-10 02:34:09 +0900

    페이지 진입시 음악재생 테스트

--- src/page/ImageTest.jsx
+++ src/page/ImageTest.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import ImageMeta from "../TestComponents/ImageMeta";
-
+import sound from "../asset/accidentSound.mp3";
 const s = {
   Frame: styled.div`
     width: 100%;
@@ -9,9 +9,28 @@ const s = {
 };
 
 const ImageTest = () => {
+  const audioRef = useRef(null);
+  const [isFlag, setIsFlag] = useState(false);
+  useEffect(() => {
+    if (isFlag && audioRef.current) {
+      audioRef.current.play(); // Play the audio when status is 200
+      audioRef.current.loop = true;
+    } else if (!isFlag) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  }, [isFlag]);
   return (
     <s.Frame>
       <ImageMeta />
+      <audio ref={audioRef} src={sound} />
+      <button
+        onClick={() => {
+          setIsFlag(!isFlag);
+        }}
+      >
+        버튼
+      </button>
     </s.Frame>
   );
 };
